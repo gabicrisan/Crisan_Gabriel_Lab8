d85cb40e4a0437ad0e1ce4752aaf3261a42aabab
GABRIEL CRIȘAN <gabriel.crisan@stud.ubbcluj.ro>
2021-01-02 15:02:16 +0200

    Add project files.

---
+++ Data/Crisan_Gabriel_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crisan_Gabriel_Lab8.Models;
+
+namespace Crisan_Gabriel_Lab8.Data
+{
+    public class Crisan_Gabriel_Lab8Context : DbContext
+    {
+        public Crisan_Gabriel_Lab8Context (DbContextOptions<Crisan_Gabriel_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crisan_Gabriel_Lab8.Models.Book> Book { get; set; }
+    }
+}
